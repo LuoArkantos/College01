093e727b830d86c4d61b4ce12a1434b9709faebf
Tomatine <luizfernandolima1997@gmail.com>
2023-08-03 14:28:26 -0300

    Subindo com erro (Não mostra lista )

---
+++ College/Frm_ListaDeAlunos.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace College
+{
+    partial class Frm_ListaDeAlunos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.clsAlunoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.nomeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nomeMaeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nomePaiDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nascimentoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telefone1DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telefone2DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ruaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cepDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.numeroDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bairroDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cidadeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ufDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clsAlunoBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clsAlunoBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clsAlunoBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nomeDataGridViewTextBoxColumn,
+            this.nomeMaeDataGridViewTextBoxColumn,
+            this.nomePaiDataGridViewTextBoxColumn,
+            this.nascimentoDataGridViewTextBoxColumn,
+            this.telefone1DataGridViewTextBoxColumn,
+            this.telefone2DataGridViewTextBoxColumn,
+            this.ruaDataGridViewTextBoxColumn,
+            this.cepDataGridViewTextBoxColumn,
+            this.numeroDataGridViewTextBoxColumn,
+            this.bairroDataGridViewTextBoxColumn,
+            this.cidadeDataGridViewTextBoxColumn,
+            this.ufDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.clsAlunoBindingSource1;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(958, 461);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // clsAlunoBindingSource
+            // 
+            this.clsAlunoBindingSource.DataSource = typeof(Cls_Uteis.Cls_Aluno);
+            // 
+            // nomeDataGridViewTextBoxColumn
+            // 
+            this.nomeDataGridViewTextBoxColumn.DataPropertyName = "Nome";
+            this.nomeDataGridViewTextBoxColumn.HeaderText = "Nome";
+            this.nomeDataGridViewTextBoxColumn.Name = "nomeDataGridViewTextBoxColumn";
+            // 
+            // nomeMaeDataGridViewTextBoxColumn
+            // 
+            this.nomeMaeDataGridViewTextBoxColumn.DataPropertyName = "NomeMae";
+            this.nomeMaeDataGridViewTextBoxColumn.HeaderText = "NomeMae";
+            this.nomeMaeDataGridViewTextBoxColumn.Name = "nomeMaeDataGridViewTextBoxColumn";
+            // 
+            // nomePaiDataGridViewTextBoxColumn
+            // 
+            this.nomePaiDataGridViewTextBoxColumn.DataPropertyName = "NomePai";
+            this.nomePaiDataGridViewTextBoxColumn.HeaderText = "NomePai";
+            this.nomePaiDataGridViewTextBoxColumn.Name = "nomePaiDataGridViewTextBoxColumn";
+            // 
+            // nascimentoDataGridViewTextBoxColumn
+            // 
+            this.nascimentoDataGridViewTextBoxColumn.DataPropertyName = "Nascimento";
+            this.nascimentoDataGridViewTextBoxColumn.HeaderText = "Nascimento";
+            this.nascimentoDataGridViewTextBoxColumn.Name = "nascimentoDataGridViewTextBoxColumn";
+            // 
+            // telefone1DataGridViewTextBoxColumn
+            // 
+            this.telefone1DataGridViewTextBoxColumn.DataPropertyName = "Telefone1";
+            this.telefone1DataGridViewTextBoxColumn.HeaderText = "Telefone1";
+            this.telefone1DataGridViewTextBoxColumn.Name = "telefone1DataGridViewTextBoxColumn";
+            // 
+            // telefone2DataGridViewTextBoxColumn
+            // 
+            this.telefone2DataGridViewTextBoxColumn.DataPropertyName = "Telefone2";
+            this.telefone2DataGridViewTextBoxColumn.HeaderText = "Telefone2";
+            this.telefone2DataGridViewTextBoxColumn.Name = "telefone2DataGridViewTextBoxColumn";
+            // 
+            // ruaDataGridViewTextBoxColumn
+            // 
+            this.ruaDataGridViewTextBoxColumn.DataPropertyName = "Rua";
+            this.ruaDataGridViewTextBoxColumn.HeaderText = "Rua";
+            this.ruaDataGridViewTextBoxColumn.Name = "ruaDataGridViewTextBoxColumn";
+            // 
+            // cepDataGridViewTextBoxColumn
+            // 
+            this.cepDataGridViewTextBoxColumn.DataPropertyName = "Cep";
+            this.cepDataGridViewTextBoxColumn.HeaderText = "Cep";
+            this.cepDataGridViewTextBoxColumn.Name = "cepDataGridViewTextBoxColumn";
+            // 
+            // numeroDataGridViewTextBoxColumn
+            // 
+            this.numeroDataGridViewTextBoxColumn.DataPropertyName = "Numero";
+            this.numeroDataGridViewTextBoxColumn.HeaderText = "Numero";
+            this.numeroDataGridViewTextBoxColumn.Name = "numeroDataGridViewTextBoxColumn";
+            // 
+            // bairroDataGridViewTextBoxColumn
+            // 
+            this.bairroDataGridViewTextBoxColumn.DataPropertyName = "Bairro";
+            this.bairroDataGridViewTextBoxColumn.HeaderText = "Bairro";
+            this.bairroDataGridViewTextBoxColumn.Name = "bairroDataGridViewTextBoxColumn";
+            // 
+            // cidadeDataGridViewTextBoxColumn
+            // 
+            this.cidadeDataGridViewTextBoxColumn.DataPropertyName = "Cidade";
+            this.cidadeDataGridViewTextBoxColumn.HeaderText = "Cidade";
+            this.cidadeDataGridViewTextBoxColumn.Name = "cidadeDataGridViewTextBoxColumn";
+            // 
+            // ufDataGridViewTextBoxColumn
+            // 
+            this.ufDataGridViewTextBoxColumn.DataPropertyName = "Uf";
+            this.ufDataGridViewTextBoxColumn.HeaderText = "Uf";
+            this.ufDataGridViewTextBoxColumn.Name = "ufDataGridViewTextBoxColumn";
+            // 
+            // clsAlunoBindingSource1
+            // 
+            this.clsAlunoBindingSource1.DataSource = typeof(Cls_Uteis.Cls_Aluno);
+            // 
+            // Frm_ListaDeTurmas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(958, 461);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Frm_ListaDeTurmas";
+            this.Text = "Frm_ListaDeTurmas";
+            this.Load += new System.EventHandler(this.Frm_ListaDeTurmas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clsAlunoBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clsAlunoBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource clsAlunoBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nomeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nomeMaeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nomePaiDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nascimentoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telefone1DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telefone2DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ruaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cepDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numeroDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bairroDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cidadeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ufDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource clsAlunoBindingSource1;
+    }
+}
---
+++ College/Frm_ListaDeAlunos.cs
@@ -0,0 +1,75 @@
+﻿using Cls_Uteis;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Shapes;
+
+namespace College
+{
+    public partial class Frm_ListaDeAlunos : Form
+    {
+        public Frm_ListaDeAlunos()
+        {
+            InitializeComponent();
+        }
+        private void Frm_ListaDeTurmas_Load(object sender, EventArgs e)
+        {
+            string localDoArquivo = "RelacaoDeAlunos.csv";
+
+
+            if (File.Exists(localDoArquivo))
+            {
+
+                string[] linhas = File.ReadAllLines(localDoArquivo);
+
+                DataGridView dgv = new DataGridView();
+
+                dgv.Rows.Clear();
+                dgv.Columns.Clear();
+
+
+                string[] columnHeaders = linhas[0].Split(';');
+
+
+                foreach (string header in columnHeaders)
+                {
+                    dgv.Columns.Add(header, header);
+                }
+
+
+                for (int i = 1; i < linhas.Length; i++)
+                {
+                    string[] values = linhas[i].Split(';');
+                    dgv.Rows.Add(values);
+                }
+            }
+            else
+            {
+
+                MessageBox.Show("File not found: " + localDoArquivo, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        //private void Frm_ListaDeTurmas_Load(object sender, EventArgs e)
+        //{
+        //    DataGridView dgv = new DataGridView();
+        //    string localDoArquivo = "RelacaoDeAlunos.csv";
+
+        //    string[] linhas = File.ReadAllLines(localDoArquivo);
+
+        //    for (int i = 0; i < linhas.Length; i++)
+        //    {
+        //        dgv.Rows.Add(linhas[1]);
+        //    }
+        //}
+    }
+
+}
